91434b8dac7a8161d1293dfc6049b8218a988a66
lkk223303 <lkk223303@gmail.com>
2022-03-23 17:58:33 +0800

    grpcClient finished

--- grpcClient.go
+++ grpcClient.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
 	"log"
 	"time"
+
+	pb "acc_service/proto"
 
 	"google.golang.org/grpc"
 )
 
-var CONNECT grpc.ClientConn
-
-// Initiate and return gRPC Client 
+// Initiate and return gRPC Client
 func grpcClientOn()*grpc.ClientConn {
+	
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
 	// 連線
 	addr := "127.0.0.1:8787"
 	conn, err := grpc.DialContext(ctx, addr, grpc.WithInsecure(), grpc.WithBlock())
@@ -33,58 +31,82 @@ func grpcClientOff(conn *grpc.ClientConn) {
 
 /////////// gRPC呼叫 gRPC server 進行資料庫操作 ///////////
 
-// 新增user資料
+// 新增user資料 done
 func grpcInsertUser(username, password string) error {
-	c := grpcClientOn()
+	client := grpcClientOn()
+	pbService := pb.NewHelloServiceClient(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
 	// 執行 InsertUser 方法
-	r, err := c.InsertUser(ctx, &pb.InsertRequest{username: username,password:password})
+	r, err := pbService.InsertUser(ctx, &pb.InsertRequest{Username: username,Password:password})
 	if err != nil {
 		log.Fatalln(err.Error())
 		return err
 	}else{
-		log.Printf("User Inserted: %s", r.GetUsername())
-		grpcClientOff(c)
+		log.Printf("User Inserted: %s", r.GetReply())
 	}
-	return err	
+
+	grpcClientOff(client)
+	return nil	
 }
 
-// 查詢 Username 並回傳結果
-func grpcQueryUser(db *sql.DB, username string) (error, *User) {
-	user := new(User)
-	row := db.QueryRow("SELECT * FROM users WHERE username=?", username)
-	if err := row.Scan(&user.ID, &user.User, &user.Password); err != nil {
-		fmt.Printf("查詢使用者失敗，原因：%v\n", err)
-		return err, user
+// 查詢 Username 並回傳結果 done
+func grpcQueryUser( username string) error {
+	client := grpcClientOn()
+	pbService := pb.NewHelloServiceClient(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// 執行 QueryUser 方法
+	r, err := pbService.QueryUser(ctx, &pb.QueryRequest{Username: username})
+	if err != nil {
+		log.Fatalln(err.Error())
+		return err
+	}else{
+		log.Printf("User found: %s", r.GetReply())
 	}
 
-	fmt.Printf("查詢使用者 ID: %s | Name: %s | Password: %s ", user.ID, user.User, user.Password)
-	return nil, user
+	grpcClientOff(client)
+	return nil	
 }
 
 // User mehtod只會刪除並回傳成功與否(不檢查)
-func grpcDeleteUser(db *sql.DB, username string) error {
+func grpcDeleteUser(username string) error {
+	client := grpcClientOn()
+	pbService := pb.NewHelloServiceClient(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-	result, err := db.Exec("DELETE FROM users WHERE username=?", username)
+	// 執行 QueryUser 方法
+	r, err := pbService.DeleteUser(ctx, &pb.DeleteRequest{Username: username})
 	if err != nil {
-		fmt.Println("Result: ", result, "Error: ", err)
+		log.Fatalln(err.Error())
+		return err
+	}else{
+		log.Printf("User delete: %s", r.GetReply())
 	}
 
-	return err // usually nil
+	grpcClientOff(client)
+	return nil
 }
 
 // 檢查是否輸入正確帳號,密碼,二次密碼，並回傳密碼更改是否成功和新密碼
-func grpcChangePassword(db *sql.DB, username, password, password2 string) {
-	var user *User
-	row := db.QueryRow("SELECT FROM users WHERE username=?", username)
-	if err := row.Scan(&user.ID, &user.User, &user.Password, &user.PasswordAgain); err != nil {
-		fmt.Printf("查詢使用者失敗，原因：%v\n", err)
+func grpcChangePassword(username, password, password2 string) error{
+	client := grpcClientOn()
+	pbService := pb.NewHelloServiceClient(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// 執行 QueryUser 方法
+	r, err := pbService.ChangePassword(ctx, &pb.ChangePasswordRequest{Username: username})
+	if err != nil {
+		log.Fatalln(err.Error())
+		return err
+	}else{
+		log.Printf("Password Changed: %s", r.GetReply())
 	}
-	if username == user.User && password == user.Password && password2 == user.PasswordAgain {
-		result, err := db.Exec("UPDATE FROM users SET password=? WHERE username=?", password, username)
-		if err != nil {
-			fmt.Println("UPDATE error: ", err)
-		} else {
-			fmt.Println("Result: ", result)
-		}
-	}
+
+	grpcClientOff(client)
+	return nil
 }
